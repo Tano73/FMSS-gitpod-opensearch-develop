14d13d23b1bd10da290ebb245b9bbacd37fce1bd
Tano73 <danilo_lanzi@yahoo.it>
2024-04-30 12:20:05 +0000

    Aggiornati scripts

--- scripts/osd-init.sh
+++ scripts/osd-init.sh
@@ -4,7 +4,7 @@ if [[ ! -f /workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards/.osd-
   nvm use 
   npm i -g yarn 
   brew install python@3.10 
-  export PYTHON=/home/linuxbrew/.linuxbrew/bin/python3.10 
+  echo "export PYTHON=/home/linuxbrew/.linuxbrew/bin/python3.10" >> $HOME/.bashrc
   cat ../osd-config.yml >> config/opensearch_dashboards.yml
   touch .osd-init.ok
 fi
--- scripts/start-os.sh
+++ scripts/start-os.sh
@@ -1,1 +1,3 @@
-docker run --rm -d --net=host -e "discovery.type=single-node" -v /workspace/FMSS-gitpod-opensearch-develop:/usr/share/opensearch/data opensearchproject/opensearch:1.2.0
+chmod 777 /workspace/FMSS-gitpod-opensearch-develop/data
+echo 'docker run --rm -d --net=host -e "discovery.type=single-node" -v /workspace/FMSS-gitpod-opensearch-develop/data:/usr/share/opensearch/data opensearchproject/opensearch:1.2.0'
+docker run --rm -d --net=host -e "discovery.type=single-node" -v /workspace/FMSS-gitpod-opensearch-develop/data:/usr/share/opensearch/data opensearchproject/opensearch:1.2.0
