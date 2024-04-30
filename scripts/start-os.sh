1c791d7aa0804d35dcb01e911a0f36f78af7e198
Tano73 <danilo_lanzi@yahoo.it>
2024-04-30 12:01:08 +0000

    Aggiornato scripts

--- scripts/start-os.sh
+++ scripts/start-os.sh
@@ -1,1 +1,1 @@
-docker run -d --net=host -e "discovery.type=single-node" opensearchproject/opensearch:1.2.0
+docker run --rm -d --net=host -e "discovery.type=single-node" -v /workspace/FMSS-gitpod-opensearch-develop:/usr/share/opensearch/data opensearchproject/opensearch:1.2.0
