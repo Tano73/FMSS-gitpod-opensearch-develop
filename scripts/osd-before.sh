aeade2f57a57a6f470e64b6c11dbc3310a58606a
Tano73 <danilo_lanzi@yahoo.it>
2024-04-30 11:54:20 +0000

    Introdotto directory scripts

---
+++ scripts/osd-before.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ ! -d "/workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards" ]]; then
+  cd /workspace/FMSS-gitpod-opensearch-develop 
+  git clone https://github.com/opensearch-project/OpenSearch-Dashboards.git 
+  cd OpenSearch-Dashboards 
+  git checkout 1.2.0 
+  cd plugins 
+  git clone https://github.com/opensearch-project/security-dashboards-plugin.git 
+  cd security-dashboards-plugin 
+  git checkout 1.2.0.0
+fi
--- start-os.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --net=host -e "discovery.type=single-node" opensearchproject/opensearch:1.2.0
