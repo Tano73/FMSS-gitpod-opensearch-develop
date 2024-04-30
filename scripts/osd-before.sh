d84648dedd95e539a6057122e1d5b19a780a1a2a
Tano73 <danilo_lanzi@yahoo.it>
2024-04-30 12:29:49 +0000

    Aggiornati scripts

--- scripts/osd-before.sh
+++ scripts/osd-before.sh
@@ -9,4 +9,8 @@ if [[ ! -d "/workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards" ]];
   git clone https://github.com/opensearch-project/security-dashboards-plugin.git 
   cd security-dashboards-plugin 
   git checkout 1.2.0.0
+  cd /workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards
+  nvm install 10.24.1
+  nvm use
+  npm i -g yarn
 fi
--- scripts/osd-init.sh
+++ scripts/osd-init.sh
@@ -1,10 +1,8 @@
 if [[ ! -f /workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards/.osd-init.ok ]]; then 
   cd /workspace/FMSS-gitpod-opensearch-develop/OpenSearch-Dashboards 
-  nvm install 10.24.1 
-  nvm use 
-  npm i -g yarn 
   brew install python@3.10 
   echo "export PYTHON=/home/linuxbrew/.linuxbrew/bin/python3.10" >> $HOME/.bashrc
+  export PYTHON=/home/linuxbrew/.linuxbrew/bin/python3.10
   cat ../osd-config.yml >> config/opensearch_dashboards.yml
   touch .osd-init.ok
 fi
